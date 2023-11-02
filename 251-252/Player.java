aea4f7da150471ef95b4b654a98c2ad3ea3f7a45
HimAtcoding <132318089+HimAtcoding@users.noreply.github.com>
2023-11-01 20:09:07 -0700

    Update Player.java

--- 251-252/Player.java
+++ 251-252/Player.java
@@ -8,7 +8,8 @@ import java.util.Scanner;
 public class Player
 {
   /* your code here - attributes */
-
+  public String name = "";
+  int points = 0;
   /* your code here - constructor(s) */ 
   public Player() {
     int points = 0;
@@ -19,12 +20,26 @@ public class Player
     System.out.println("Welcome to the game, " + name + "!");
   }
 
-  public Player(String inputname) {
+  public Player(String inputName) {
     String name = inputname;
     int points = 0;
     System.out.println("Hello there, and welcome to the game, " + name);
   }
-  /* your code here - accessor(s) */ 
 
+  /* your code here - accessor(s) */ 
+  public String getName() {
+    return name;
+  }
+
+  public int getPoints() {
+    return points;
+  }
   /* your code here - mutator(s) */ 
+  public void setName(String inputName) {
+    //incomplete
+  }
+
+  public void addToPoints(int value) {
+    //incomplete
+  }
 }

e03e94f625b2795aa9ed69f311b530626127f9c8
HimAtcoding <132318089+HimAtcoding@users.noreply.github.com>
2023-11-05 15:38:09 -0800

    Update Player.java

--- 251-252/Player.java
+++ 251-252/Player.java
@@ -1,45 +1,39 @@
-/*
- * Activity 2.5.2
- * 
- * A Player class the PhraseSolverGame
- */
 import java.util.Scanner;
 
-public class Player
-{
-  /* your code here - attributes */
-  public String name = "";
-  int points = 0;
-  /* your code here - constructor(s) */ 
-  public Player() {
-    int points = 0;
-    System.out.println("Enter player name: ");
-    Scanner sc = new Scanner(System.in);
-    String name = sc.nextLine();
+public class Player {
+  public String name;
+  int points1; // Points for player 1
+  int points2; // Points for player 2
 
+  public Player() {
+    points1 = 0;
+    points2 = 0;
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter player name: ");
+    name = sc.nextLine();
     System.out.println("Welcome to the game, " + name + "!");
   }
 
   public Player(String inputName) {
-    String name = inputname;
-    int points = 0;
+    name = inputName;
+    points1 = 0;
+    points2 = 0;
     System.out.println("Hello there, and welcome to the game, " + name);
   }
 
-  /* your code here - accessor(s) */ 
   public String getName() {
     return name;
   }
 
-  public int getPoints() {
-    return points;
-  }
-  /* your code here - mutator(s) */ 
-  public void setName(String inputName) {
-    //incomplete
+  public int getPoints(int playerNumber) {
+    return (playerNumber == 1) ? points1 : points2;
   }
 
-  public void addToPoints(int value) {
-    //incomplete
+  public void addToPoints(int playerNumber, int value) {
+    if (playerNumber == 1) {
+      points1 += value;
+    } else {
+      points2 += value;
+    }
   }
 }

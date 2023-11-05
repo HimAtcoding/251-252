f7c73a1f731ae2d5b44c922c905803628281ece4
HimAtcoding <132318089+HimAtcoding@users.noreply.github.com>
2023-11-05 15:36:52 -0800

    Update PhraseSolver.java

--- 251-252/PhraseSolver.java
+++ 251-252/PhraseSolver.java
@@ -25,21 +25,41 @@ public class PhraseSolver
 
   public void play()
   {
-    boolean solved = false;
     int currentPlayer = 1;
 
     Scanner input = new Scanner(System.in);
 
     boolean correct = true;
-    while (!solved) 
-    {
+    while (!solved) {
+      // Print the current solved phrase
+      System.out.println("Current Solved Phrase: " + board.getSolvedPhrase());
 
-      /* your code here - game logic */
+      Player currentPlayerObject = (currentPlayer == 1) ? player1 : player2;
+      String currentPlayerName = currentPlayerObject.getName();
 
+      System.out.print(currentPlayerName + ", enter your guess: ");
+      String guess = input.nextLine();
 
-      /* your code here - determine how game ends */
-      solved = true; 
-    } 
+      if (guess.length() == 1) {
+        boolean foundLetter = board.guessLetter(guess);
+        if (foundLetter) {
+          int randomPoints = (int) (Math.random() * 5) + 1; // Generate a random point value
+          currentPlayerObject.addToPoints(currentPlayer, randomPoints); // Award points for guessing a correct letter
+          System.out.println(currentPlayerName + " guessed a correct letter and earned " + randomPoints + " points.");
+          System.out.println("Player 1 points: " + player1.getPoints(1));
+          System.out.println("Player 2 points: " + player2.getPoints(2));
+        }
+      }
+
+      if (board.isSolved(board.getSolvedPhrase())) {
+        System.out.println(currentPlayerName + " solved the phrase!");
+        solved = true;
+        break;
+      }
+
+      System.out.println("Incorrect guess. Switching players.");
+      currentPlayer = (currentPlayer == 1) ? 2 : 1;
+    }
 
   }
 
